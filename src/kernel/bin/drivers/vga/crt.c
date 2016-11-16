6fa012c3b71114e8595dfcf8a96aa38873170d53
Thomas Zimmermann <tdz@users.sourceforge.net>
2016-11-16 14:05:48 +0100

    kernel/vga: Fix coding style

--- src/kernel/bin/drivers/vga/crt.c
+++ src/kernel/bin/drivers/vga/crt.c
@@ -21,81 +21,75 @@
 #include <stddef.h>
 #include "ioports.h"
 
-enum
-{
-        MAX_ROW = 25,
-        MAX_COL = 80
+enum {
+    MAX_ROW = 25,
+    MAX_COL = 80
 };
 
 static unsigned long
 crt_getindex(unsigned short row, unsigned short col)
 {
-        return col + row * MAX_COL;
+    return col + row * MAX_COL;
 }
 
 ssize_t
 crt_write(volatile unsigned char *vidmem,
           const void *buf, size_t count, unsigned char attr)
 {
-        size_t i;
-        const unsigned char *buf8;
+    size_t i;
+    const unsigned char *buf8;
 
-        for (buf8 = buf, i = 0; i < count; ++i)
-        {
-                *vidmem = *buf8;
-                ++buf8;
-                ++vidmem;
-                *vidmem = attr;
-                ++vidmem;
-        }
+    for (buf8 = buf, i = 0; i < count; ++i) {
+        *vidmem = *buf8;
+        ++buf8;
+        ++vidmem;
+        *vidmem = attr;
+        ++vidmem;
+    }
 
-        return i;
+    return i;
 }
 
 int
 crt_getpos(unsigned short *row, unsigned short *col)
 {
-        unsigned long curh, curl, cur;
+    unsigned long curh = io_inb_index(0x3d4, 0x0e, 0x03d5);
+    unsigned long curl = io_inb_index(0x3d4, 0x0f, 0x03d5);
 
-        curh = io_inb_index(0x3d4, 0x0e, 0x03d5);
-        curl = io_inb_index(0x3d4, 0x0f, 0x03d5);
+    unsigned long cur = (curh << 8) | curl;
 
-        cur = (curh << 8) | curl;
+    *row = cur / 80;
+    *col = cur - (*row) * 80;
 
-        *row = cur / 80;
-        *col = cur - (*row) * 80;
-
-        return 0;
+    return 0;
 }
 
 int
 crt_getmaxpos(unsigned short *row, unsigned short *col)
 {
-        *row = MAX_ROW;
-        *col = MAX_COL;
+    *row = MAX_ROW;
+    *col = MAX_COL;
 
-        return 0;
+    return 0;
 }
 
 int
 crt_setpos(unsigned short row, unsigned short col)
 {
-        unsigned long cur = crt_getindex(row, col);
+    unsigned long cur = crt_getindex(row, col);
 
-        io_outb_index(0x03d4, 0x0e, 0x03d5, (cur >> 8) & 0xff);
-        io_outb_index(0x03d4, 0x0f, 0x03d5, cur & 0xff);
+    io_outb_index(0x03d4, 0x0e, 0x03d5, (cur >> 8) & 0xff);
+    io_outb_index(0x03d4, 0x0f, 0x03d5, cur & 0xff);
 
-        return 0;
+    return 0;
 }
 
 volatile unsigned char *
 crt_getaddress(unsigned short row, unsigned short col)
 {
-        if ((row >= MAX_ROW) || (col >= MAX_COL))
-        {
-                return NULL;
-        }
+    if ((row >= MAX_ROW) || (col >= MAX_COL)) {
+        return NULL;
+    }
 
-        return ((volatile unsigned char *)0xb8000) + 2 * crt_getindex(row,
-                                                                      col);
+    return ((volatile unsigned char *)0xb8000) + 2 * crt_getindex(row, col);
 }

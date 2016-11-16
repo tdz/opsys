d15e49cf81e4b2482c6c8bd41730c1dd3d29d67b
Thomas Zimmermann <tdz@users.sourceforge.net>
2016-11-16 14:35:12 +0100

    kernel: Fix coding style in ioports.c

--- src/kernel/bin/arch/i386/ioports.c
+++ src/kernel/bin/arch/i386/ioports.c
@@ -22,31 +22,41 @@
 void
 io_outb(unsigned short port, unsigned char byte)
 {
-__asm__("mov %1, %%al\n\t" "mov %0, %%dx\n\t" "out %%al, %%dx\n":
-: "r"(port), "r"(byte):"%al", "%dx");
+    __asm__("mov %1, %%al\n\t"
+            "mov %0, %%dx\n\t"
+            "out %%al, %%dx\n"
+            :
+            : "r"(port), "r"(byte)
+            : "%al", "%dx");
 }
 
 void
 io_outb_index(unsigned short iport, unsigned char index,
               unsigned short dport, unsigned char byte)
 {
-        io_outb(iport, index);
-        io_outb(dport, byte);
+    io_outb(iport, index);
+    io_outb(dport, byte);
 }
 
 unsigned char
 io_inb(unsigned short port)
 {
-        unsigned char byte;
+    unsigned char byte;
 
-__asm__("mov %1, %%dx\n\t" "in %%dx, %%al\n\t" "mov %%al, %0\n": "=r"(byte): "r"(port):"%al", "%dx");
+    __asm__("mov %1, %%dx\n\t"
+            "in %%dx, %%al\n\t"
+            "mov %%al, %0\n"
+            : "=r"(byte)
+            : "r"(port)
+            : "%al", "%dx");
 
-        return byte;
+    return byte;
 }
 
 unsigned char
 io_inb_index(unsigned short iport, unsigned char index, unsigned short dport)
 {
-        io_outb(iport, index);
-        return io_inb(dport);
+    io_outb(iport, index);
+
+    return io_inb(dport);
 }
